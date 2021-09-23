70380beec4c64f0624c05df4f3c005e2d5e99d90
Clement De dessus <clement.dedessuslesmous@student.vinci.be>
2021-09-23 13:45:15 +0200

    exercice 2.2

--- fiche-01/dom-event/index.js
+++ fiche-01/dom-event/index.js
@@ -1,13 +1,10 @@
-var nombreClics = 0;
-
-function comptage() {
-  nombreClics++;
-  document.getElementById("nombreClics").textContent   = nombreClics;
-}
-
-document.getElementById("boutonClic").addEventListener("click", comptage);
-
-document.getElementById("desactiver").addEventListener("click", function(e) {
-    document.getElementById("boutonClic").removeEventListener("click", comptage);
+let click = document.querySelector("#boutonClic");
+click.addEventListener("click", event => {
+  click.innerText = event.detail;
+  if (event.detail === 5) {
+    click.innerHTML = "Bravo, bel échauffement !"
+  }
+  if (event.detail === 10) {
+    click.innerHTML = "Vous êtes passé maître en l’art du clic !"
+  }
 });
-
