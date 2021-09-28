6152f1adf523b444acaf604e57b01891e00bdf70
Clement De dessus <clement.dedessuslesmous@student.vinci.be>
2021-09-28 08:12:54 +0200

    add - function clock and pause

--- fiche-01/time-events/index.js
+++ fiche-01/time-events/index.js
@@ -1,2 +1,27 @@
-const dateTimeNow = new Date(); 
-console.log(dateTimeNow.toLocaleTimeString());
+let boutton = document.getElementById("myBtn");
+let horloge = setInterval(clock, 1000);
+let booleanSwitch = true;
+boutton.style.backgroundColor = "red";
+boutton.style.color = "white";
+
+function clock() {
+  let d = new Date();
+  let t = d.toLocaleTimeString();
+  document.getElementById("horloge").innerText = t;
+}
+
+function pause() {
+  if (booleanSwitch) {
+    clearInterval(horloge);
+    booleanSwitch = false;
+    boutton.innerText = "Start";
+    boutton.style.backgroundColor = "blue";
+  } else {
+    horloge = setInterval(clock, 1000);
+    booleanSwitch = true;
+    boutton.innerText = "Stop";
+    boutton.style.backgroundColor = "red";
+  }
+}
+
+boutton.addEventListener("click", pause);
