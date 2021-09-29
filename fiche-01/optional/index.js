a0f568866fd55eabcf28764d20406021dc52ae38
Clement De dessus <clement.dedessuslesmous@student.vinci.be>
2021-09-29 12:01:30 +0200

    add - mouseover image

--- fiche-01/optional/index.js
+++ fiche-01/optional/index.js
@@ -1,11 +1,16 @@
-// source : https://www.pierre-giraud.com/afficher-cacher-div-javascript/
-
 let partie1 = document.getElementById("partie1");
 let partie2 = document.getElementById("partie2");
+let partie3 = document.getElementById("partie3");
+let partie4 = document.getElementById("partie4");
 
 partie1.style.color = "green";
 partie2.style.color = "blue";
+//partie3.style.color = "black";
 
+//mouseover pour le texte 
 partie1.addEventListener("mouseover", () => {partie2.style.display = "block";});
 partie1.addEventListener("mouseout", () => {partie2.style.display = "none";});
 
+//mouseover pour l'image 
+partie3.addEventListener("mouseover", () => {partie4.style.display = "block";});
+partie3.addEventListener("mouseout", () => {partie4.style.display = "none";});
