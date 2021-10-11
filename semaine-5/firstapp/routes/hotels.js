9504c94f6e9843232cc58e6a4a04fc6f097effd3
Clement De dessus <clement.dedessuslesmous@student.vinci.be>
2021-10-11 14:37:05 +0200

    add - firstapp

---
+++ semaine-5/firstapp/routes/hotels.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var router = express.Router();
+
+const HOTELS = [
+  { id: 1, name: "Hilton BXL", stars: 4 },
+  { id: 2, name: "Prestige", stars: 5 },
+];
+
+/* Read all hotels */
+router.get("/", function (req, res, next) {
+  return res.json(HOTELS);
+});
+
+/* Create an hotels */
+router.post("/", function (req, res, next) {
+  if (!req.body || !req.body.name || !req.body.stars)
+    return res.sendStatus(400);
+
+  const newHotel = {
+    id: HOTELS.length + 1,
+    name: req.body.name,
+    stars: req.body.stars,
+  };
+
+  HOTELS.push(newHotel);
+  return res.json(newHotel);
+});
+
+module.exports = router;
