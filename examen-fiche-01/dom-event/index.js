94b9d0975f45e13f6100d76c7d29179abbbd6e7b
Clement De dessus <clement.dedessuslesmous@student.vinci.be>
2021-12-21 12:36:23 +0100

    refactor - dom-event

--- examen-fiche-01/dom-event/index.js
+++ examen-fiche-01/dom-event/index.js
@@ -1,11 +1,24 @@
 let click = document.querySelector("#boutonClic");
 
-click.addEventListener("click", event => {
+click.addEventListener("click", (event) => {
   click.innerHTML = event.detail;
-  if (event.detail === 5){
-    click.innerHTML = "Bravo, bel échauffement ! "
+  if (event.detail === 5) {
+    click.innerHTML = "Bravo, bel échauffement ! ";
   }
   if (event.detail === 10) {
-    click.innerHTML = "Vous êtes passé maître en l’art du clic ! "
+    click.innerHTML = "Vous êtes passé maître en l’art du clic ! ";
   }
+  if (event.detail === 20) {
+    click.innerHTML = "Vous êtes un champion ! ";
+  }
+});
+
+let d1 = document.getElementById("d1");
+d1.addEventListener("mouseover", () => {
+  p1.style.display = "block";
+  p2.style.display = "block";
+});
+d1.addEventListener("mouseout", () => {
+  p1.style.display = "none";
+  p2.style.display = "none";
 });
