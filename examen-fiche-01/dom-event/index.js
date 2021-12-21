36a3c1a52925c27df1971a520834a01bd40951cd
Clement De dessus <clement.dedessuslesmous@student.vinci.be>
2021-12-21 11:35:12 +0100

    add - dom-event

---
+++ examen-fiche-01/dom-event/index.js
@@ -0,0 +1,11 @@
+let click = document.querySelector("#boutonClic");
+
+click.addEventListener("click", event => {
+  click.innerHTML = event.detail;
+  if (event.detail === 5){
+    click.innerHTML = "Bravo, bel échauffement ! "
+  }
+  if (event.detail === 10) {
+    click.innerHTML = "Vous êtes passé maître en l’art du clic ! "
+  }
+});
