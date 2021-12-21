8507a9b4b240f21d435c2a99e2bb76ed8208314a
Clement De dessus <clement.dedessuslesmous@student.vinci.be>
2021-12-21 12:20:17 +0100

    add - time-events

---
+++ examen-fiche-01/time-events/index.js
@@ -0,0 +1,27 @@
+let boutton = document.getElementById("myBtn");
+let horloge = setInterval(clock, 1000);
+let booleanSwitch = true;
+boutton.style.backgroundColor = "red";
+boutton.style.color = "white";
+
+function clock() {
+  let d = new Date();
+  let t = d.toLocaleTimeString();
+  document.getElementById("horloge").innerText = t;
+}
+
+function pause() {
+  if (booleanSwitch) {
+    clearInterval(horloge);
+    booleanSwitch = false;
+    boutton.innerText = "Start";
+    boutton.style.backgroundColor = "green";
+  } else {
+    horloge = setInterval(clock, 1000);
+    booleanSwitch = true;
+    boutton.innerText = "Stop";
+    boutton.style.backgroundColor = "red";
+  }
+}
+
+boutton.addEventListener("click", pause);
